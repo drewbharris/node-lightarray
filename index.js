eccb8c04867edffae69d247a8d8a7ff0c5f3ed8f
drew harris <drewbharris@gmail.com>
2013-06-04 20:55:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ var midi = require('midi'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
+    
+var version = 1,
+    values = [0, 0, 0, 0];
+
+// Web app routes
 
 app.get('/', function(req, res){
     var output = new midi.output();
     var name = output.getPortName(1);
     res.send(name);
+});
+
+// API routes
+
+app.get('/api/v' + version + '/array', function(req, res){
+    res.json({
+        'values': values
+    });
+});
+
+app.get('/api/v' + version + '/array/:element', function(req, res){
+    res.send(values[req.params.element]);
+});
+
+app.post('/api/v' + version + '/array/:element', function(req, res){
+    
 });
 
 io.sockets.on('connection', function (socket) {
