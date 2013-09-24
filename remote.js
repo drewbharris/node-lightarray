f61d7b21a1fb7287496f760f58a624d128eacd86
Drew Harris <drewbharris@gmail.com>
2013-09-24 14:58:10 +0000

    change remote to Linux device name, fix a bug

--- remote.js
+++ remote.js
@@ -3,8 +3,8 @@ var io = require('socket.io').listen(8001),
     mdns = require('mdns');
 
 var lightArray = new LightArray({
-    'serialPort': '/dev/tty.usbmodem15001',
-    'debug': true,
+    'serialPort': '/dev/ttyACM0',
+    'debug': false,
     'sockets': {}
 });
 
@@ -14,7 +14,7 @@ lightArray.on('ready', function(){
     io.sockets.on('connection', function (socket) {
         console.log('connection from host');
         socket.on('updateAll', function (data) {
-            lightArray.updateAll(data);
+            lightArray.writeToArduino(data.values);
         });
     });
     ad = mdns.createAdvertisement(new mdns.ServiceType('lightarray', 'tcp'), 8001);
